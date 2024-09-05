e18575f3da3d645531cf7c10e5993ac32290dca5
as-1-earth <149356063+as-1-earth@users.noreply.github.com>
2024-09-05 17:38:55 +0900

    お試し

--- Assets/script.cs
+++ Assets/script.cs
@@ -7,7 +7,7 @@ public class script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("aaa");
     }
 
     // Update is called once per frame
