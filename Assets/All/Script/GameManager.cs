db9d7b0a8b6ec1120b7dd1d5de78f794d5ca6ac2
as-1-earth <149356063+as-1-earth@users.noreply.github.com>
2024-09-05 19:29:54 +0900

    ステージ作成

---
+++ Assets/All/Script/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+namespace UnityEngine
+{
+    public class GameManager : MonoBehaviour
+    {
+        public GameObject _stage;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
